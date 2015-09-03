0b5806efca9d5bb3057c2572065dab630d6b589d
qomolangmaice <qomolangmaice@163.com>
2015-09-04 00:42:53 +0800

    Try to figure out process module

--- boot/boot.s
+++ boot/boot.s
@@ -43,18 +43,3 @@ start:
 	jmp $                       ; Enter an infinite loop, to stop the processor
                                 ; executing whatever rubbish is in the memory
                                 ; after our kernel!
-
-;restart: 
-;	mov esp, [p_proc_ready] 	 	 	; Next process address that will be started  
-;	lldt [esp + P_LDT_SEL] 
-;	lea eax, [esp, P_STACKTOP] 
-;	mov dword [tss + TSS3_S_SP0], eax 
-
-;	pop gs 
-;	pop fs 
-;	pop es 
-;	pop ds 
-;	popad 
-;	add esp, 4 
-;	iretd 
-
